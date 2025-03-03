9750aa8719f41e306d533fc0991c8c1ad8fffa64
severogianluca <gianlucasevero300@gmail.com>
2025-03-03 16:04:29 +0100

    Feat: create snake 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function iniziali(nomi, input){
+    const newList = [];
+    for(let i = 0 ; i < nomi.length; i++){
+        const list = nomi[i];
+        if(list[0].toLowerCase() === input.toLowerCase())
+        newList.push(list)
+    }
+    return newList;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = iniziali(names, 'a')
+console.log(result)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
